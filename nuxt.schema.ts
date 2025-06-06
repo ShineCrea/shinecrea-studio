bb481bf45abb4d8402716e67edeb4a9e19607ad6
Amandine Vrn <amandinevrne@gmail.com>
2025-06-06 16:46:41 +0200

    Update nuxt.schema.ts

--- nuxt.schema.ts
+++ nuxt.schema.ts
@@ -1,14 +1,8 @@
-import defineStudioSchema from '@nuxthq/studio'
-import defineField from '@nuxthq/studio/defineField'
-import defineGroup from '@nuxthq/studio/defineGroup'
-
-export default defineStudioSchema({}, {
-  article: defineGroup({
-    title: 'Articles',
-    fields: {
-      title: defineField({ type: 'string' }),
-      content: defineField({ type: 'textarea' }),
-      coverImage: defineField({ type: 'media' }),
-    }
-  })
+export default defineNuxtConfig({
+  modules: [
+    '@nuxt/content',
+    '@nuxthq/studio'
+  ],
+  devtools: { enabled: true },
+  compatibilityDate: '2025-05-15' 
 })
