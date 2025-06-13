5adc8d4ac7bd5c79d887bfc3dba6740c3e701ec1
Amandine Vrn <amandinevrne@gmail.com>
2025-06-13 11:08:05 +0200

    Update nuxt.schema.ts

--- nuxt.schema.ts
+++ nuxt.schema.ts
@@ -1,41 +1,21 @@
+import { field, group } from '@nuxt/content/preview'
+
 export default defineNuxtSchema({
-  ui: group({
-    title: 'UI',
-    description: 'UI Customization.',
-    icon: 'i-mdi-palette-outline',
-    fields: {
-      primary: field({
-        type: 'string',
-        title: 'Primary',
-        description: 'Primary color of your UI.',
-        icon: 'i-mdi-palette-outline',
-        required: true,
-        options: [
-          'sky',
-          'mint',
-          'rose',
-          'amber',
-          'violet',
-          'emerald',
-          'fuchsia'
-        ]
-      }),
-      gray: field({
-        // définis ici les propriétés nécessaires
-      }),
-      icons: group({
-        title: 'Icons',
-        description: 'Manage icons used in UI Pro.',
-        icon: 'i-mdi-application-settings-outline',
-        fields: {
-          search: field({ /* ... */ }),
-          dark: field({ /* ... */ }),
-          light: field({ /* ... */ }),
-          external: field({ /* ... */ }),
-          chevron: field({ /* ... */ }),
-          hash: field({ /* ... */ })
-        }
-      })
-    }
-  })
+  appConfig: {
+    parent: group({
+      title: 'UI',
+      description: 'UI configuration',
+      icon: 'i-ph-palette-fill',
+      fields: {
+        primary: field({
+          type: 'string',
+          title: 'Primary',
+          description: 'Primary color of your UI.',
+          icon: 'i-ph-palette',
+          default: 'sky',
+          required: ['sky', 'mint', 'rose', 'amber']
+        })
+      }
+    })
+  }
 })
